0492b01a0bd417e21bf451e47f72f6dd624c3a8c
LisaMure <140246111+LisaMure@users.noreply.github.com>
2023-09-12 19:47:30 +0200

    Updated the weather container's html to make it mobile responsive. Merged the components into one component

--- src/CurrentLocation.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function CurrentLocation() {
-  return (
-    <button className="btn btn-success w-100 current-location-btn">
-      Current Location
-    </button>
-  );
-}
--- src/Date.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Date() {
-  return (
-    <p className="date">
-      Last updated <span>Saturday, 18:36</span>
-    </p>
-  );
-}
